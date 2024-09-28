22f583922069561827411504ff8be75526d1b625
Jackma <jackmavelor@gmail.com>
2024-09-28 21:39:03 +0300

    Added files

---
+++ main.py
@@ -0,0 +1,51 @@
+import stackclass as st
+import os
+from time import sleep
+obj=st.Stack()
+option=None
+value=None
+num_delete=None
+element_index=None
+index=None
+while option!=0:
+    print()
+    print("Choose an operation from the Menu below")
+    print("Input 0 to exit")
+    print("")
+    print("1.Add elements into the stack")
+    print("2.Remove elements from the stack")
+    print("3.Count of elements in the stack")
+    print("4.Display stack elements")
+    print("5.Check element stored in a certain position")
+    option=int(input("Your option"))
+    if option==1:
+        value=int(input("Enter an integral value to be stored in the stack"))
+        obj.push(value)
+        sleep(1)
+        print()
+    elif option==2:
+        num_delete=int(input("Input element to be deleted"))
+        element_index=obj.pop(num_delete)
+        if element_index!=-1:
+            print(F"The value {num_delete} which was at position {element_index} of the stack has been removed successfully")
+        else:
+            print("Stack underflow-The stack is empty")
+        sleep(1)
+        print()
+    elif option==3:
+        print(f'The stack has {obj.count()} elements')
+        sleep(1)
+        print()
+    elif option==4:
+        obj.display()
+        sleep(1)
+        print()
+    elif option==5:
+        index=int(input("Input stack position of the element to be checked"))
+        obj.peek(index)
+        sleep(1)
+        print()
+    else:
+        print(F"{option} is an invalid option.Input a proper one")
+        sleep(1)
+        print()
---
+++ stackclass.py
@@ -0,0 +1,46 @@
+class Stack:
+    def __init__(self):
+        self.array_capacity=10
+        self.data=[]
+    def count(self):
+        return len(self.data)
+    def push(self,value):
+        if len(self.data)==0:
+            self.data.append(value)
+            print(F"The value {value} has been successfully pushed into the stack")
+            return
+        elif len(self.data)==self.array_capacity:
+            print("Stack overflow-The stack is full")
+            return
+        else:
+            self.data.append(value)
+            print(F"The value {value} has been successfully pushed into the stack")
+            return
+    def pop(self,value1):
+        if self.array_capacity==0:
+            return -1
+        else:
+            for t in range(0,len(self.data),1):
+                if value1==self.data[t]:
+                    self.data.remove(value1)
+                    return t
+
+    def peek(self,pos):
+        for w in range(0,len(self.data),1):
+            if pos==w:
+                print(F"The element at index {pos} of the stack is {self.data[w]}")
+                return
+        else:
+            print(F"The index {pos} is out of bounds")
+            return
+
+    def display(self):
+        if len(self.data)==0:
+            print("The stack is empty")
+        else:
+            num=-1
+            print("Stack elements:-")
+            while num>=-(len(self.data)):
+                print(self.data[num],end=",")
+                num-=1
+        return
